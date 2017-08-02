8c54ca1e1b0f6676f14b257d985cd9b3c6903945
Baginska <paulina.baginska@capgemini.com>
2017-08-02 09:39:46 +0200

    Pierwszy commit

---
+++ App1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        private int zmienna1;
+        public Program(int _zmienna)
+        { zmienna1 = _zmienna; }
+        //public Zmienna1 
+        //    {get=>zmienna1; set=>name=value; };
+
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
---
+++ DrugaKlasa.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Class1
+{
+    private int zmienna2;
+	public Class1()
+	{
+	}
+}
